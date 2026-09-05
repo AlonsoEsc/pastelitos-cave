f617f878a46c1ea496df79a890c0e706b8ff502e
AlonsoEsc <lonsoesc99@gmia.com>
2026-09-05 14:55:00 -0600

    Add custom favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,10 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Pastelito’s cave 😗🤭',
   description: 'Un rincón hecho con amor para Mariela Alejandra Cruz Aguirre.',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
